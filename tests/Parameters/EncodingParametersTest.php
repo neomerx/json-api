a2bfb48fa0fa903a300feaad2a18aa5269e2f2bb
EK <ek@neomerx.com>
2015-05-10 18:10:50 +0300

    Add tests to EncodingParameters

---
+++ tests/Parameters/EncodingParametersTest.php
@@ -0,0 +1,60 @@
+<?php namespace Neomerx\Tests\JsonApi\Parameters;
+
+/**
+ * Copyright 2015 info@neomerx.com (www.neomerx.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use \Mockery;
+use \Neomerx\Tests\JsonApi\BaseTestCase;
+use \Neomerx\JsonApi\Parameters\EncodingParameters;
+
+/**
+ * @package Neomerx\Tests\JsonApi
+ */
+class EncodingParametersTest extends BaseTestCase
+{
+    /**
+     * Test null encoding parameters.
+     */
+    public function testNulls()
+    {
+        $parameters = new EncodingParameters(null, null);
+
+        $this->assertNull($parameters->getFieldSets());
+        $this->assertNull($parameters->getIncludePaths());
+        $this->assertNull($parameters->getFieldSet('whatever'));
+        $this->assertTrue($parameters->isPathIncluded('any.path'));
+    }
+
+    /**
+     * Test not null encoding parameters.
+     */
+    public function testNotNulls()
+    {
+        $parameters = new EncodingParameters(
+            $paths = ['some.path'],
+            $fieldsets = ['type' => ['field1', 'field2']]
+        );
+
+        $this->assertEquals($fieldsets, $parameters->getFieldSets());
+        $this->assertEquals($paths, $parameters->getIncludePaths());
+        $this->assertEquals([], $parameters->getFieldSet('typeNotInSet'));
+        $this->assertEquals($fieldsets['type'], $parameters->getFieldSet('type'));
+        $this->assertTrue($parameters->isPathIncluded('some.path'));
+        $this->assertFalse($parameters->isPathIncluded('some'));
+        $this->assertFalse($parameters->isPathIncluded('some.path.plus'));
+        $this->assertFalse($parameters->isPathIncluded('completely.different'));
+    }
+}
