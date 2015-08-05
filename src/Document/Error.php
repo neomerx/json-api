653637a1f9e4b0de2ee3b5cff674e769c0973098
Adam Lavin <adam@lavoaster.co.uk>
2015-08-05 11:28:16 +0100

    Accept integers for $status and $code in Error document constructor and cast them to string.

--- src/Document/Error.php
+++ src/Document/Error.php
@@ -87,15 +87,16 @@ class Error implements ErrorInterface
     ) {
         assert(
             '($idx === null || is_int($idx) || is_string($idx)) &&'.
-            '($status === null || is_string($status)) && ($code === null || is_string($code)) &&'.
+            '($status === null || is_int($status) || is_string($status)) &&'.
+            '($code === null || is_int($code) || is_string($code)) &&'.
             '($title === null  || is_string($title)) && ($title === null || is_string($title)) &&'.
             '($detail === null || is_string($detail)) && ($meta === null || is_array($meta))'
         );
 
         $this->idx     = $idx;
         $this->links   = ($aboutLink === null ? null : [DocumentInterface::KEYWORD_ERRORS_ABOUT => $aboutLink]);
-        $this->status  = $status;
-        $this->code    = $code;
+        $this->status  = (string) $status;
+        $this->code    = (string) $code;
         $this->title   = $title;
         $this->source  = $source;
         $this->detail  = $detail;
