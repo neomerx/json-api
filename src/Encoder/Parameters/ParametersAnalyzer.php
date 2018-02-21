ba10aeba56098edc4935b21193dfddda16114517
SirLamer <greg.j.peden@gmail.com>
2018-02-21 02:47:56 -0700

    Correct relationship mismatch  case
    Given a model 'issue-event' with relationships 'issue' and 'issue-event-type', if you try to query the model 'issue-event' with an include request for 'issue-event-type', the current version will accidentally also match against 'issue' only while bundling the JSON API output but not during the database query itself. The result is that it will include an empty result for 'issue' with null values for attributes and relationships, like so:
    
    ```json
    {
        "data": {
            "type": "issue-events",
            "id": "177",
            "attributes": {
                "created-at": "2018-02-20T21:55:43-05:00",
                "updated-at": "2018-02-20T21:55:43-05:00"
            },
            "relationships": {
                "device": {
                    "data": {
                        "type": "devices",
                        "id": "62"
                    }
                },
                "issue": {
                    "data": {
                        "type": "issues",
                        "id": "105"
                    }
                },
                "issue-event-type": {
                    "data": {
                        "type": "issue-event-types",
                        "id": "1"
                    }
                },
                "user": {
                    "data": {
                        "type": "users",
                        "id": "2"
                    }
                }
            },
            "links": {
                "self": "http:\/\/hoistway.test\/api\/v1\/issue-events\/177"
            }
        },
        "included": [{
            "type": "issues",
            "id": "105",
            "attributes": {
                "subject": null,
                "created-at": null,
                "updated-at": null,
                "closed-at": null
            },
            "relationships": {
                "devices": {
                    "data": [{
                        "type": "devices",
                        "id": "62"
                    }]
                },
                "inspection": {
                    "data": null
                },
                "inspection-area": {
                    "data": null
                },
                "issue-comments": {
                    "data": []
                },
                "issue-events": {
                    "data": [{
                        "type": "issue-events",
                        "id": "177"
                    }]
                },
                "user": {
                    "data": null
                }
            }
        }, {
            "type": "issue-event-types",
            "id": "1",
            "attributes": {
                "name": "Open",
                "slug": "open"
            },
            "relationships": {
                "issue-events": []
            }
        }]
    }
    ```
    
    This change includes the dot separator in the comparison check to make sure a match only occurs on a proper parent relationship.

--- src/Encoder/Parameters/ParametersAnalyzer.php
+++ src/Encoder/Parameters/ParametersAnalyzer.php
@@ -168,7 +168,7 @@ class ParametersAnalyzer implements ParametersAnalyzerInterface, LoggerAwareInte
 
         if ($path !== null) {
             foreach ($paths as $targetPath) {
-                if (strpos($targetPath, $path) === 0) {
+                if (strpos($targetPath, $path . DocumentInterface::PATH_SEPARATOR) === 0) {
                     $hasMatch = true;
                     break;
                 }
