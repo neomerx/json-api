7d5e51f22c298ff858626f966a1a035d41d557fc
EK <ek@neomerx.com>
2018-03-14 18:11:55 +0300

    Close #198

--- tests/Http/Query/BaseQueryParserTest.php
+++ tests/Http/Query/BaseQueryParserTest.php
@@ -17,8 +17,15 @@
  */
 
 use Generator;
+use Neomerx\JsonApi\Contracts\Http\Query\BaseQueryParserInterface;
+use Neomerx\JsonApi\Encoder\Encoder;
+use Neomerx\JsonApi\Encoder\Parameters\EncodingParameters;
 use Neomerx\JsonApi\Http\Query\BaseQueryParser;
 use Neomerx\Tests\JsonApi\BaseTestCase;
+use Neomerx\Tests\JsonApi\Data\Author;
+use Neomerx\Tests\JsonApi\Data\AuthorSchema;
+use Neomerx\Tests\JsonApi\Data\Comment;
+use Neomerx\Tests\JsonApi\Data\CommentSchema;
 
 /**
  * @package Neomerx\Tests\JsonApi
@@ -211,11 +218,188 @@ class BaseQueryParserTest extends BaseTestCase
     }
 
     /**
+     * Shows how to integrate base query parser with EncodingParameters.
+     *
+     * @return void
+     *
+     * @see https://github.com/neomerx/json-api/issues/198
+     */
+    public function testIntegrationWithEncodingParameters(): void
+    {
+        $queryParameters = [
+            BaseQueryParser::PARAM_FIELDS  => [
+                'comments' => Comment::LINK_AUTHOR . ',     ' . Comment::ATTRIBUTE_BODY . '      ',
+                'people'   => Author::ATTRIBUTE_FIRST_NAME,
+            ],
+            BaseQueryParser::PARAM_SORT    => '-created,title,+updated',
+            BaseQueryParser::PARAM_INCLUDE => Comment::LINK_AUTHOR . ',   ' .
+                Comment::LINK_AUTHOR . '.' . Author::LINK_COMMENTS,
+        ];
+
+        // It is expected that classes that encapsulate/extend BaseQueryParser would add features
+        // such filters/pagination parsing, validation, etc. Though for simplicity we omit adding
+        // them here and check how it integrates with EncodingParameters.
+        $parser = new class ($queryParameters) extends BaseQueryParser
+        {
+            /**
+             * @var null|array
+             */
+            private $fields = null;
+
+            /**
+             * @var null|array
+             */
+            private $sorts = null;
+
+            /**
+             * @var null|array
+             */
+            private $includes = null;
+
+            /**
+             * @return array
+             */
+            public function getFields(): array
+            {
+                if ($this->fields === null) {
+                    $this->fields = $this->iterableToArray(parent::getFields());
+                }
+
+                return $this->fields;
+            }
+
+            /**
+             * @return array
+             */
+            public function getSorts(): array
+            {
+                if ($this->sorts === null) {
+                    $this->sorts = $this->iterableToArray(parent::getSorts());;
+                }
+
+                return $this->sorts;
+            }
+
+            /**
+             * @return array
+             */
+            public function getIncludes(): array
+            {
+                if ($this->includes === null) {
+                    $this->includes = array_keys($this->iterableToArray(parent::getIncludes()));
+                }
+
+                return $this->includes;
+            }
+
+            /**
+             * @param iterable $iterable
+             *
+             * @return array
+             */
+            private function iterableToArray(iterable $iterable): array
+            {
+                $result = [];
+
+                foreach ($iterable as $key => $value) {
+                    $result[$key] = $value instanceof Generator ? $this->iterableToArray($value) : $value;
+                }
+
+                return $result;
+            }
+        };
+
+        // Check parsing works fine
+        $this->assertSame([
+            'comments' => [Comment::LINK_AUTHOR, Comment::ATTRIBUTE_BODY],
+            'people'   => [Author::ATTRIBUTE_FIRST_NAME],
+        ], $parser->getFields());
+        $this->assertSame([
+            'created' => false,
+            'title'   => true,
+            'updated' => true,
+        ], $parser->getSorts());
+        $this->assertSame([
+            Comment::LINK_AUTHOR,
+            Comment::LINK_AUTHOR . '.' . Author::LINK_COMMENTS,
+        ], $parser->getIncludes());
+
+        //
+        // Now the main purpose of the test. Will it work with EncodingParameters?
+        //
+
+        // firstly setup some data
+        $author                          = Author::instance(9, 'Dan', 'Gebhardt');
+        $comments                        = [
+            Comment::instance(5, 'First!', $author),
+            Comment::instance(12, 'I like XML better', $author),
+        ];
+        $author->{Author::LINK_COMMENTS} = $comments;
+
+        // and encode with params taken from the parser
+        $encodingParams = new EncodingParameters($parser->getIncludes(), $parser->getFields());
+        $actual         = Encoder::instance([
+            Author::class  => AuthorSchema::class,
+            Comment::class => CommentSchema::class,
+        ])->encodeData($comments, $encodingParams);
+
+        $expected = <<<EOL
+        {
+          "data": [
+            {
+              "type": "comments",
+              "id": "5",
+              "attributes": {
+                "body": "First!"
+              },
+              "relationships": {
+                "author": {
+                  "data": { "type": "people", "id": "9" }
+                }
+              },
+              "links": {
+                "self": "/comments/5"
+              }
+            },
+            {
+              "type": "comments",
+              "id": "12",
+              "attributes": {
+                "body": "I like XML better"
+              },
+              "relationships": {
+                "author": {
+                  "data": { "type": "people", "id": "9" }
+                }
+              },
+              "links": {
+                "self": "/comments/12"
+              }
+            }
+          ],
+          "included": [
+            {
+              "type": "people",
+              "id": "9",
+              "attributes":{
+                "first_name":"Dan"
+              }
+            }
+          ]
+        }
+EOL;
+        // remove formatting from 'expected'
+        $expected = json_encode(json_decode($expected));
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
      * @param array $queryParameters
      *
-     * @return BaseQueryParser
+     * @return BaseQueryParserInterface
      */
-    private function createParser(array $queryParameters): BaseQueryParser
+    private function createParser(array $queryParameters): BaseQueryParserInterface
     {
         return new BaseQueryParser($queryParameters);
     }
