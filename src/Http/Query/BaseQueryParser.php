5fd74568a3ef52bb53351ecb66a824bad1c22802
EK <ek@neomerx.com>
2018-03-15 16:59:56 +0300

    Close #204

--- src/Http/Query/BaseQueryParser.php
+++ src/Http/Query/BaseQueryParser.php
@@ -17,14 +17,18 @@
  */
 
 use Neomerx\JsonApi\Contracts\Http\Query\BaseQueryParserInterface;
-use Neomerx\JsonApi\Document\Error;
-use Neomerx\JsonApi\Exceptions\JsonApiException;
 
 /**
  * @package Neomerx\JsonApi
  */
 class BaseQueryParser implements BaseQueryParserInterface
 {
+    use BaseQueryParserTrait {
+        BaseQueryParserTrait::getFields as getFieldsImpl;
+        BaseQueryParserTrait::getIncludes as getIncludesImpl;
+        BaseQueryParserTrait::getSorts as getSortsImpl;
+    }
+
     /** Message */
     public const MSG_ERR_INVALID_PARAMETER = 'Invalid Parameter.';
 
@@ -44,39 +48,7 @@ class BaseQueryParser implements BaseQueryParserInterface
      */
     public function __construct(array $parameters = [], array $messages = null)
     {
-        $this->setParameters($parameters);
-        $this->messages = $messages;
-    }
-
-    /**
-     * @param array $parameters
-     *
-     * @return self
-     */
-    public function setParameters(array $parameters): self
-    {
-        $this->parameters = $parameters;
-
-        return $this;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getIncludes(): iterable
-    {
-        if (array_key_exists(static::PARAM_INCLUDE, $this->getParameters()) === true) {
-            $splitByDot = function (string $path): iterable {
-                foreach ($this->splitStringAndCheckNoEmpties(static::PARAM_INCLUDE, $path, '.') as $link) {
-                    yield $link;
-                }
-            };
-
-            $includes = $this->getParameters()[static::PARAM_INCLUDE];
-            foreach ($this->splitCommaSeparatedStringAndCheckNoEmpties(static::PARAM_INCLUDE, $includes) as $path) {
-                yield $path => $splitByDot($path);
-            }
-        }
+        $this->setParameters($parameters)->setMessages($messages);
     }
 
     /**
@@ -84,16 +56,15 @@ class BaseQueryParser implements BaseQueryParserInterface
      */
     public function getFields(): iterable
     {
-        if (array_key_exists(static::PARAM_FIELDS, $this->getParameters()) === true) {
-            $fields = $this->getParameters()[static::PARAM_FIELDS];
-            if (is_array($fields) === false || empty($fields) === true) {
-                throw new JsonApiException($this->createParameterError(static::PARAM_FIELDS));
-            }
+        return $this->getFieldsImpl($this->getParameters(), $this->getMessage(static::MSG_ERR_INVALID_PARAMETER));
+    }
 
-            foreach ($fields as $type => $fieldList) {
-                yield $type => $this->splitCommaSeparatedStringAndCheckNoEmpties($type, $fieldList);
-            }
-        }
+    /**
+     * @inheritdoc
+     */
+    public function getIncludes(): iterable
+    {
+        return $this->getIncludesImpl($this->getParameters(), $this->getMessage(static::MSG_ERR_INVALID_PARAMETER));
     }
 
     /**
@@ -101,75 +72,19 @@ class BaseQueryParser implements BaseQueryParserInterface
      */
     public function getSorts(): iterable
     {
-        if (array_key_exists(static::PARAM_SORT, $this->getParameters()) === true) {
-            $sorts = $this->getParameters()[static::PARAM_SORT];
-            foreach ($this->splitCommaSeparatedStringAndCheckNoEmpties(static::PARAM_SORT, $sorts) as $orderAndField) {
-                switch ($orderAndField[0]) {
-                    case '-':
-                        $isAsc = false;
-                        $field = substr($orderAndField, 1);
-                        break;
-                    case '+':
-                        $isAsc = true;
-                        $field = substr($orderAndField, 1);
-                        break;
-                    default:
-                        $isAsc = true;
-                        $field = $orderAndField;
-                        break;
-                }
-
-                yield $field => $isAsc;
-            }
-        }
+        return $this->getSortsImpl($this->getParameters(), $this->getMessage(static::MSG_ERR_INVALID_PARAMETER));
     }
 
     /**
-     * @param string       $paramName
-     * @param string|mixed $shouldBeString
-     * @param string       $separator
+     * @param array $parameters
      *
-     * @return iterable
+     * @return self
      */
-    protected function splitString(string $paramName, $shouldBeString, string $separator): iterable
+    protected function setParameters(array $parameters): self
     {
-        if (is_string($shouldBeString) === false || ($trimmed = trim($shouldBeString)) === '') {
-            throw new JsonApiException($this->createParameterError($paramName));
-        }
+        $this->parameters = $parameters;
 
-        foreach (explode($separator, $trimmed) as $value) {
-            yield $value;
-        }
-    }
-
-    /**
-     * @param string       $paramName
-     * @param string|mixed $shouldBeString
-     * @param string       $separator
-     *
-     * @return iterable
-     */
-    protected function splitStringAndCheckNoEmpties(string $paramName, $shouldBeString, string $separator): iterable
-    {
-        foreach ($this->splitString($paramName, $shouldBeString, $separator) as $value) {
-            $trimmedValue = trim($value);
-            if (($trimmedValue) === '') {
-                throw new JsonApiException($this->createParameterError($paramName));
-            }
-
-            yield $trimmedValue;
-        }
-    }
-
-    /**
-     * @param string       $paramName
-     * @param string|mixed $shouldBeString
-     *
-     * @return iterable
-     */
-    protected function splitCommaSeparatedStringAndCheckNoEmpties(string $paramName, $shouldBeString): iterable
-    {
-        return $this->splitStringAndCheckNoEmpties($paramName, $shouldBeString, ',');
+        return $this;
     }
 
     /**
@@ -181,28 +96,15 @@ class BaseQueryParser implements BaseQueryParserInterface
     }
 
     /**
-     * @param string $parameterName
+     * @param array $messages
      *
-     * @return Error
+     * @return self
      */
-    protected function createParameterError(string $parameterName): Error
+    protected function setMessages(?array $messages): self
     {
-        return $this->createQueryError($parameterName, static::MSG_ERR_INVALID_PARAMETER);
-    }
+        $this->messages = $messages;
 
-    /**
-     * @param string $name
-     * @param string $title
-     *
-     * @return Error
-     */
-    protected function createQueryError(string $name, string $title): Error
-    {
-        $title  = $this->getMessage($title);
-        $source = [Error::SOURCE_PARAMETER => $name];
-        $error  = new Error(null, null, null, null, $title, null, $source);
-
-        return $error;
+        return $this;
     }
 
     /**
---
+++ src/Http/Query/BaseQueryParserTrait.php
@@ -0,0 +1,160 @@
+<?php namespace Neomerx\JsonApi\Http\Query;
+
+/**
+ * Copyright 2015-2018 info@neomerx.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use Neomerx\JsonApi\Contracts\Document\ErrorInterface;
+use Neomerx\JsonApi\Contracts\Http\Query\BaseQueryParserInterface as P;
+use Neomerx\JsonApi\Document\Error;
+use Neomerx\JsonApi\Exceptions\JsonApiException;
+
+/**
+ * @package Neomerx\JsonApi
+ */
+trait BaseQueryParserTrait
+{
+    /**
+     * @param array  $parameters
+     * @param string $errorTitle
+     *
+     * @return iterable
+     */
+    protected function getIncludes(array $parameters, string $errorTitle): iterable
+    {
+        if (array_key_exists(P::PARAM_INCLUDE, $parameters) === true) {
+            $splitByDot = function (string $path) use ($errorTitle): iterable {
+                foreach ($this->splitStringAndCheckNoEmpties(P::PARAM_INCLUDE, $path, '.', $errorTitle) as $link) {
+                    yield $link;
+                }
+            };
+
+            $paramName = P::PARAM_INCLUDE;
+            $includes  = $parameters[P::PARAM_INCLUDE];
+            foreach ($this->splitCommaSeparatedStringAndCheckNoEmpties($paramName, $includes, $errorTitle) as $path) {
+                yield $path => $splitByDot($path);
+            }
+        }
+    }
+
+    /**
+     * @param array  $parameters
+     * @param string $errorTitle
+     *
+     * @return iterable
+     */
+    protected function getFields(array $parameters, string $errorTitle): iterable
+    {
+        if (array_key_exists(P::PARAM_FIELDS, $parameters) === true) {
+            $fields = $parameters[P::PARAM_FIELDS];
+            if (is_array($fields) === false || empty($fields) === true) {
+                throw new JsonApiException($this->createParameterError(P::PARAM_FIELDS, $errorTitle));
+            }
+
+            foreach ($fields as $type => $fieldList) {
+                yield $type => $this->splitCommaSeparatedStringAndCheckNoEmpties($type, $fieldList, $errorTitle);
+            }
+        }
+    }
+
+    /**
+     * @param array  $parameters
+     * @param string $errorTitle
+     *
+     * @return iterable
+     */
+    protected function getSorts(array $parameters, string $errorTitle): iterable
+    {
+        if (array_key_exists(P::PARAM_SORT, $parameters) === true) {
+            $sorts  = $parameters[P::PARAM_SORT];
+            $values = $this->splitCommaSeparatedStringAndCheckNoEmpties(P::PARAM_SORT, $sorts, $errorTitle);
+            foreach ($values as $orderAndField) {
+                switch ($orderAndField[0]) {
+                    case '-':
+                        $isAsc = false;
+                        $field = substr($orderAndField, 1);
+                        break;
+                    case '+':
+                        $isAsc = true;
+                        $field = substr($orderAndField, 1);
+                        break;
+                    default:
+                        $isAsc = true;
+                        $field = $orderAndField;
+                        break;
+                }
+
+                yield $field => $isAsc;
+            }
+        }
+    }
+
+    /**
+     * @param string       $paramName
+     * @param string|mixed $shouldBeString
+     * @param string       $errorTitle
+     *
+     * @return iterable
+     */
+    private function splitCommaSeparatedStringAndCheckNoEmpties(
+        string $paramName,
+        $shouldBeString,
+        string $errorTitle
+    ): iterable {
+        return $this->splitStringAndCheckNoEmpties($paramName, $shouldBeString, ',', $errorTitle);
+    }
+
+    /**
+     * @param string       $paramName
+     * @param string|mixed $shouldBeString
+     * @param string       $separator
+     * @param string       $errorTitle
+     *
+     * @return iterable
+     */
+    private function splitStringAndCheckNoEmpties(
+        string $paramName,
+        $shouldBeString,
+        string $separator,
+        string $errorTitle
+    ): iterable {
+        if (is_string($shouldBeString) === false || ($trimmed = trim($shouldBeString)) === '') {
+            throw new JsonApiException($this->createParameterError($paramName, $errorTitle));
+        }
+
+        foreach (explode($separator, $trimmed) as $value) {
+            $trimmedValue = trim($value);
+            if (($trimmedValue) === '') {
+                throw new JsonApiException($this->createParameterError($paramName, $errorTitle));
+            }
+
+            yield $trimmedValue;
+        }
+    }
+
+    /**
+     * @param string $paramName
+     * @param string $errorTitle
+     *
+     * @return ErrorInterface
+     */
+    private function createParameterError(string $paramName, string $errorTitle): ErrorInterface
+    {
+        $source = [Error::SOURCE_PARAMETER => $paramName];
+        $error  = new Error(null, null, null, null, $errorTitle, null, $source);
+
+        return $error;
+    }
+}
